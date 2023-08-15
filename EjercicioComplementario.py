fbb5a7706ebf14ad8557283915aadb11e4238b67
Santiago <ojedasi2004@gmail.com>
2023-08-15 19:02:50 -0300

    Primer Commit

---
+++ EjercicioComplementario.py
@@ -0,0 +1,38 @@
+#1
+numero1 = 1
+#2
+numero2 = 1.5
+#3
+suma = numero1 + numero2 
+#4 
+resta = numero1 - numero2 
+multi =  numero1 * numero2
+divi = numero1 / numero2
+print(resta,multi,divi)
+#5
+nombre = "Santiago"
+#6
+precio = 150
+#7
+descuento = 0.1
+#8
+precio_final = precio - (precio * descuento)
+#9
+cadena = "Boca quiere la septima"
+#10
+longitud = len(cadena)
+#11
+precio=int(10.15)
+#12
+nombre = "Santiago "
+apellido = "Ojeda"
+nombre_completo = nombre + apellido
+#13 
+edad = 15+5-10
+#14
+altura = 1.63 * 4 / 3
+#15
+var1 = "SANTIAGO".lower()
+print(var1)
+#16
+var2 = "SANTIAGO".lower().title()
